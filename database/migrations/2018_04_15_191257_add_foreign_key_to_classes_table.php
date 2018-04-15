951ca5594521208f36657efbffce8dfcddc1c3ae
Amr Hassan <amrh434@gmail.com>
2018-04-15 21:26:06 +0200

    migration : foreign keys for classes and records table .

---
+++ database/migrations/2018_04_15_191257_add_foreign_key_to_classes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKeyToClassesTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('classes', function (Blueprint $table) {
+			$table->foreign('user_id')
+				->references('id')->on('users')
+				->onDelete('cascade');
+			$table->foreign('course_id')
+				->references('id')->on('courses')
+				->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('classes', function (Blueprint $table) {
+
+		});
+	}
+}
