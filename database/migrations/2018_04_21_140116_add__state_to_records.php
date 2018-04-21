1aee305db11af99e0bb35e128fcae57c95b636fd
Amr Hassan <amrh434@gmail.com>
2018-04-21 16:03:49 +0200

    migration : state col to records table to determine the state of the course wither it's paid or not .

---
+++ database/migrations/2018_04_21_140116_add__state_to_records.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStateToRecords extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('records', function (Blueprint $table) {
+            $table->enum('state', ['paid', 'postponed']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('records', function (Blueprint $table) {
+            $table->dropColumn('state');
+        });
+    }
+}
