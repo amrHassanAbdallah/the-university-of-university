9b2390620fdc76ca16622acaf113526d4fb597ef
Amr Hassan <amrh434@gmail.com>
2018-04-28 23:28:44 +0200

    seed : added  seed for the class join for the main teacher , also added default user level for user seed  .

--- database/seeds/ClassSeeder.php
+++ database/seeds/ClassSeeder.php
@@ -15,21 +15,38 @@ class ClassSeeder extends Seeder
     {
 
         $faker = new Faker();
-
+        factory('App\User', 10)->create(['level' => 'student']);
         $coursesIDs = DB::table('courses')->pluck('id')->toArray();
-        $studentsIDs = DB::table('users')->where('level', 'teacher')->pluck('id')->toArray();
+        $teachersIds = DB::table('users')->where('level', 'teacher')->pluck('id')->toArray();
+        $teacher_id = \App\User::where('email', 'teacher@teacher.com')->first()->id;
+        $course_id = $coursesIDs[array_rand($coursesIDs, 1)];
+        $teacher = \App\User::find($teacher_id);
+        $course = \App\Course::find($course_id);
+        $class = \App\StudentClass::create([
+            'user_id' => $teacher_id,
+            'course_id' => $coursesIDs[array_rand($coursesIDs, 1)],
+            'date' => 'sunday at 9am',
+            'location' => 1 . 'th floor',
+            'name' => $course->name . ' by ' . $teacher->name,
+        ]);
         foreach (range(1, 10) as $index) {
-            $teacher_id = $studentsIDs[array_rand($studentsIDs, 1)];
+            $teacher_id = $teachersIds[array_rand($teachersIds, 1)];
             $course_id = $coursesIDs[array_rand($coursesIDs, 1)];
             $teacher = \App\User::find($teacher_id);
             $course = \App\Course::find($course_id);
             DB::table('classes')->insert([
-                'user_id' => $studentsIDs[array_rand($studentsIDs, 1)],
+                'user_id' => $teachersIds[array_rand($teachersIds, 1)],
                 'course_id' => $coursesIDs[array_rand($coursesIDs, 1)],
                 'date' => 'sunday at 9am',
                 'location' => 1 . 'th floor',
                 'name' => $course->name . ' by ' . $teacher->name,
             ]);
+            $studentsIds = \App\User::where('level', 'student')->get();
+            /*            $class = \App\StudentClass::where('user_id',$teacher_id)->first();*/
+            foreach ($studentsIds as $studentsId) {
+                $class->User()->save($studentsId);
+            }
+
         }
 
     }
